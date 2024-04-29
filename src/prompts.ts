9d91a579092f2631f56d6984d832a5630d0271c8
Riceball LEE <snowyu.lee@gmail.com>
2024-04-29 18:29:58 +0800

    feat(AIPrompts): add custom method getPrompt to get a better prompt for LLM

--- src/prompts.ts
+++ src/prompts.ts
@@ -2,6 +2,8 @@ import path from 'path'
 import { KVSqliteResFuncParams, KVSqliteResFunc } from "@isdk/ai-tool-sqlite";
 import { AIPromptSettings } from './prompt-settings';
 import { getConfigs } from './config';
+import { AIPromptFitResult, promptIsFitForLLM } from './prompt';
+import { CommonError, ErrorCode } from '@isdk/ai-tool';
 
 // const PROMPTS_DB_NAME = '.promptsdb'
 // const eventBus = event.runSync()
@@ -15,5 +17,27 @@ export class AIPromptsFunc extends KVSqliteResFunc<AIPromptsFuncParams> {
     const dirname = path.resolve(path.dirname(new URL(import.meta.url).pathname), '..', 'prompts');
     const configs = getConfigs(dirname)
     this.db.bulkDocs(configs);
+  }
+
+  _getPrompt(modelName: string) {
+    const db = this.db
+    const prompts = db.list() as AIPromptSettings[]
+    for (const prompt of prompts) {
+      const version = promptIsFitForLLM(prompt, modelName)
+      if (version) {
+        return {
+          prompt,
+          version,
+        }
+      }
+    }
+  }
+
+  $getPrompt({model}: AIPromptsFuncParams) {
+    if (!model) {
+      throw new CommonError('model is required', 'AIPromptsFunc.getPrompt', ErrorCode.InvalidArgument)
+    }
+    const result = this._getPrompt(model)
+    return result
   }
 }
